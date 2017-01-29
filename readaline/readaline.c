ee4e4b287441a34278873a89e65934b2c01c7f6f
Ashton C. Stephens <asteph06@lab120e.eecs.tufts.edu>
2017-01-29 16:31:52 -0500

    finishing readaline

--- readaline/readaline.c
+++ readaline/readaline.c
@@ -1,81 +1,109 @@
 /*
+ *      readaline.c
+ *      by Ashton Stephens and Rui Qin, 1/28/17
+ *      Files, Pictures and Interfaces
+ *      
+ *      reads a line from a file and places it in a character array.
  */
 
 #include "readaline.h"
 #include <stdlib.h>
 #include <stdio.h>
 
-#define TRUE 1
-#define FALSE 0
+char at_EOF        ( FILE *inputfd );
+void check_input   ( FILE *inputfd, char **datapp );
+int  expand_if_full( char **linepp, int  capacity, int num );
+void set_size      ( char **linepp, int  new_capacity ); 
+void runtime_error ( char *error );
 
-char valid_input( FILE *inputfd, char **datapp );
-int expand_line( char *old_array, int old_cap );
-void checked_runtime_error();
+/* --------------------------------------------------------*
+ * |                    Main Function
+ * --------------------------------------------------------*/
 
-size_t readaline(FILE *inputfd, char **datapp ) 
-{
+/* takes in a file pointer and a pointer to a char array and
+ * fills the array with the next line of file. Returns the 
+ * number bytes at the location pointed to by *datapp. relies
+ * on client freeing memory allocated to *datapp.
+ */
+size_t readaline( FILE *inputfd, char **datapp ) 
+{        
         int i = 0;
-        int capacity = 100;
-        char *line = malloc( sizeof(char)*capacity );
-        if ( !valid_input( inputfd, datapp  ) ){
-                checked_runtime_error();
-                return 0;
-        }else if ( feof( inputfd ) ) {
-                printf("END OF FILE\n");
+        int capacity = 10;
+        char *linep = malloc( sizeof( char ) * capacity ); 
+
+        check_input( inputfd, datapp );
+        if ( at_EOF( inputfd ) ) {
+                free( linep );
                 *datapp = NULL;
                 return 0;
         } else {
-                line[0] = '0'; 
-                do {                
-                
-                        line[i] = fgetc( inputfd );
-                        if ( i >= capacity ) {
-                                capacity = expand_line( line, capacity );
-                        }
-                        
-
-                } while ( line[i++] != '\n' );
-        
-                /*!!!*/ 
-                line[i] = '\0';
-                /*!!!*/
-                
-                (*datapp) = line; 
-                return sizeof(char) * i;
+                do {               
+                        capacity = expand_if_full( &linep, capacity, i );
+                        linep[i] = fgetc( inputfd );
+                } while ( linep[ i++ ] != '\n' );
+                set_size ( &linep, i );
+                *datapp = linep; 
+                return sizeof( char ) * i;
         }
 }
 
-/* ------------------------------------------------------  */
+/* --------------------------------------------------------*
+ * |                     Helper Functions
+ * --------------------------------------------------------*/
 
-char valid_input( FILE *inputfd, char **datapp )
+/* Returns a non zero value if the next character the file
+ * will return on a call of fgetc() is EOF
+ */
+char at_EOF ( FILE *inputfd )
 {
-        if ( inputfd == NULL || datapp == NULL ) {
-                return FALSE;
+        char temp;
+        temp = fgetc( inputfd );
+        ungetc( temp, inputfd );
+        return ( temp == EOF );
+}
+
+/* Checks the function input values for the readaline function.
+ * If inputs are invalid, terminates the program with a failure.
+ */
+void check_input( FILE *inputfd, char **datapp )
+{
+        if ( inputfd == NULL) {
+                runtime_error( "readaline called on null FILE pointer" );
+        } else if (datapp == NULL ) {
+                runtime_error( "readaline called on null char* pointer" );
         }
-        return TRUE;
 }
 
-int expand_line( char *old_array, int old_cap )
+/* Expands the array pointed to by linepp if num is the same
+ * size as the capacity. returns the new capacity of the array.
+ */
+int expand_if_full( char **linepp, int capacity, int num )
 {
-        int  new_cap = old_cap * 2;
-        char *new_array = malloc( sizeof(char) * new_cap );
-         
-        int i;
-        for( i = 0; i < old_cap; i++ ){
-                new_array[i] = old_array[i];
+        if ( num == capacity ) {
+                capacity *= 2;
+                set_size( linepp, capacity );       
         }
-        
-        free( old_array );
-        old_array = new_array;
-        return new_cap;
+        return capacity;
 }
 
-void checked_runtime_error()
+/* Sets the size of the array pointed to by linepp to be new_capacity.
+ * expects a non-zero value for new_capacity
+ */
+void set_size ( char **linepp, int new_capacity ) 
 {
+        *linepp = realloc( *linepp, sizeof( char ) * new_capacity ); 
+        if ( *linepp == NULL ) {
+                runtime_error( 
+                "readaline experienced memory allocation fail" );
+        }
 }
 
-
-
-
+/* prints error to stderr and exits the program with a failure.
+ */
+void runtime_error( char *error )
+{
+        fprintf( stderr, "%s\n", error );
+        exit( EXIT_FAILURE );
+}
 
 
